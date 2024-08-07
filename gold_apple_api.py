3da7d75b402f3be3ff8e22833f40e0fa3baba714
Velveronica <31802035+Velveronica@users.noreply.github.com>
2024-08-07 16:07:48 +0700

    Add files via upload

--- gold_apple_api.py
+++ gold_apple_api.py
@@ -1,14 +1,1 @@
 
-import pytest
-from selenium import webdriver
-from time import sleep
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-
-class Formtest:
-    def __init__(self, browser) -> None:
-        
-        self.browser=browser
-        self.browser.get("https://bonigarcia.dev/selenium-webdriver-java/data-types.html")
